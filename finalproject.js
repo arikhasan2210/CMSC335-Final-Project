960966ae8de4ffd72c1ae038decbe8946f32117f
ntekle10 <teklenathan10@gmail.com>
2025-05-13 13:58:51 -0400

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -2,14 +2,17 @@ const path = require("path")
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const portNumber=4000;
 require("dotenv").config({
-   path: path.resolve(__dirname, "enver/.env"),
+   path: path.resolve(__dirname, ".env"),
 });
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.set("views", path.resolve(__dirname, "htmlFiles"))
+app.set("views", path.resolve(__dirname, "templates"));
 
 app.get("/", (request, response) => {
-    response.render("Intro")
+    response.render("homePage")
 })
+app.listen(portNumber);
+console.log(`Web Server started and running at http://localhost:${portNumber}`);
